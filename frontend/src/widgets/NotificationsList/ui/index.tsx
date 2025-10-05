12ccb0db98b6becaf9e690fce52dcca02493f4a0
Ivan Rybin <rybin1van@outlook.com>
2025-10-05 20:08:32 +0300

    [frontend] fix: Notifications ui

--- frontend/src/widgets/NotificationsList/ui/index.tsx
+++ frontend/src/widgets/NotificationsList/ui/index.tsx
@@ -2,8 +2,8 @@
 
 import useDebounce from "@shared/hooks/useDebounce";
 import { SHOW_NOTIFICATION_DELAY } from "@utils/constants";
+import { ReactElement, ReactNode, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ReactElement, useEffect } from "react";
 import { Message } from "primereact/message";
 import { TRootState } from "@store/index";
 import {
@@ -12,34 +12,49 @@ import {
 } from "@store/slices/Notifications";
 import "./style.css";
 
+const severityIcons: Record<string, string> = {
+    success: "✔️",
+    info: "ℹ️",
+    warn: "⚠️",
+    error: "❌",
+};
+
 const NotificationsList = (): ReactElement => {
     const dispatch = useDispatch();
-
     const notifications: TNotification[] = useSelector(
         (state: TRootState) => state.notifications.notifications
     );
-
     const { runDebounced } = useDebounce(SHOW_NOTIFICATION_DELAY);
 
     useEffect(() => {
-        if (!notifications.length) {
-            return;
-        }
-
-        const firstNotificationId: string = notifications[0].id;
-
+        if (!notifications.length) return;
+        const firstNotificationId = notifications[0].id;
         runDebounced(() => {
             dispatch(clearNotificationById(firstNotificationId));
         }, false);
     }, [notifications, runDebounced, dispatch]);
 
     return (
-        <div className={`notifications-list typography-body`}>
-            {notifications.map((notification: TNotification) => (
+        <div className="notifications-list">
+            {notifications.map((notification) => (
                 <Message
                     key={notification.id}
                     severity={notification.severity}
-                    text={notification.text}
+                    content={
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                gap: "0.5rem",
+                            }}
+                        >
+                            <span className="app-message-icon">
+                                {severityIcons[notification.severity!]}
+                            </span>
+                            <span>{notification.text as ReactNode}</span>
+                        </div>
+                    }
+                    className={`app-message-root ${notification.severity}`}
                 />
             ))}
         </div>
